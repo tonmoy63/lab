e6cc2fbd99eec262564ee8252643896411c75dfb
TONMOY HALDER <107830059+tonmoy63@users.noreply.github.com>
2022-06-23 13:56:21 +0530

    Add files via upload

---
+++ multiplytwonumber.java
@@ -0,0 +1,10 @@
+public class multiplytwonumbrer {
+
+   public static void main(String[] args) {
+        
+      int a = 5, b = 15, multiply;
+      multiply = a * b;
+
+      System.out.println("Multiplication of these numbers: "+multiply);
+   }
+}
