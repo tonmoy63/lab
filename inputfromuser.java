92c1ad2cb5d04e534fab150f555ea39a4caefa99
TONMOY HALDER <107830059+tonmoy63@users.noreply.github.com>
2022-06-23 13:56:37 +0530

    Add files via upload

---
+++ inputfromuser.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; 
+class inputfromuser {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+    String userName;
+    System.out.println("Enter username"); 
+    userName = myObj.nextLine();   
+       
+    System.out.println("Username is: " + userName);        
+  }
+}
