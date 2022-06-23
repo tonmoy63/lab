710aaa8bd6f4835d18d94ffe9513fbe36e9c9fc5
TONMOY HALDER <107830059+tonmoy63@users.noreply.github.com>
2022-06-23 13:56:04 +0530

    Add files via upload

---
+++ multiplytwonumbers.java
@@ -0,0 +1,12 @@
+public class multiplytwonumbers {
+
+    public static void main(String[] args) {
+
+        float  a= 1.5f;
+        float  b= 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
+}
