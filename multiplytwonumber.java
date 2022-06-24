7403f57eab3c5ae56cfc9f9d8d4e9b2e3fe97e6e
TONMOY HALDER <107830059+tonmoy63@users.noreply.github.com>
2022-06-24 10:58:45 +0530

    Update multiplytwonumber.java

--- multiplytwonumber.java
+++ multiplytwonumber.java
@@ -1,9 +1,12 @@
+// Author: Tonmoy halder
+
 public class multiplytwonumbrer {
 
    public static void main(String[] args) {
         
-      int a = 5, b = 15, multiply;
-      multiply = a * b;
+      int a = 5, //using int data type and variable name is a
+     int b = 15, multiply; //using int data type and variable name is b
+      multiply = a * b; //multiplying two numbers
 
       System.out.println("Multiplication of these numbers: "+multiply);
    }
