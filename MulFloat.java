af9a4c54dcbaf0e17ef3821bd064df34dcc5456b
TONMOY HALDER <107830059+tonmoy63@users.noreply.github.com>
2022-06-24 11:22:34 +0530

    Update MulFloat.java

--- MulFloat.java
+++ MulFloat.java
@@ -4,8 +4,8 @@ public class MulFloat {
 
     public static void main(String[] args) {
 
-        float  a= 1.5f; //using int float type and variable name is a
-        float  b= 2.0f; //using int float type and variable name is b
+        float  a= 1.5f; //using float data type and variable name is a
+        float  b= 2.0f; //using  float data type and variable name is b
 
         float product = first * second; //Multiplying two floating numbers
 
