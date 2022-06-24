af09271e53f4ba547806e0ff5032337850f10a65
TONMOY HALDER <107830059+tonmoy63@users.noreply.github.com>
2022-06-24 11:02:01 +0530

    Update inputfromuser.java

--- inputfromuser.java
+++ inputfromuser.java
@@ -1,11 +1,13 @@
-import java.util.Scanner; 
+//Author: Tonmoy Halder
+
+import java.util.Scanner; //Import the scanner class
 class inputfromuser {
   public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);
+    Scanner myObj = new Scanner(System.in); //Create a Scanner object
     String userName;
     System.out.println("Enter username"); 
-    userName = myObj.nextLine();   
+    userName = myObj.nextLine();  //Read user input 
        
-    System.out.println("Username is: " + userName);        
+    System.out.println("Username is: " + userName);  //Output user input      
   }
 }
