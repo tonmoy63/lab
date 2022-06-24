30ab74a51cc804c00490d96179645d03600f99bd
TONMOY HALDER <107830059+tonmoy63@users.noreply.github.com>
2022-06-24 11:18:09 +0530

    Update complexnumber.java

--- complexnumber.java
+++ complexnumber.java
@@ -1,15 +1,16 @@
 class ComplexNumber {
+//variables to hold real and imaginary part of complex number
 
     int real, image;
-  
-   
+  //Constructor which will be used while creating complex number
+ 
     public ComplexNumber(int r, int i)
     {
         this.real = r;
         this.image = i;
     }
   
-   
+   // function to print real number
     public void showC()
     {
         System.out.print(this.real + " +i" + this.image);
@@ -19,33 +20,33 @@ class ComplexNumber {
     public static ComplexNumber add(ComplexNumber n1,
                                     ComplexNumber n2)
     {
-  
-      
+   // creating blank complex number to store result
+    
         ComplexNumber res = new ComplexNumber(0, 0);
- 
+        // adding real parts
         res.real = n1.real + n2.real;
   
         // adding imaginary parts
         res.image = n1.image + n2.image;
   
-
+        // returning result
         return res;
     }
   
     public static void main(String arg[])
     {
-  
+       // creating two complex numbers
         ComplexNumber c1 = new ComplexNumber(4, 5);
         ComplexNumber c2 = new ComplexNumber(10, 5);
-  
-          System.out.print("first Complex number: ");
+       
+          System.out.print("first Complex number: ");//print complex number
         c1.showC();
           
         System.out.print("\nSecond Complex number: ");
         c2.showC();
   
        
-        ComplexNumber res = add(c1, c2);
+        ComplexNumber res = add(c1, c2);// addition
   
   
         System.out.println("\nAddition is :");
