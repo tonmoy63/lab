5de38284cf729dfaf841362cd766344086debcdc
TONMOY HALDER <107830059+tonmoy63@users.noreply.github.com>
2022-06-24 10:55:57 +0530

    Update addtwonumbers.java

--- addtwonumbers.java
+++ addtwonumbers.java
@@ -1,9 +1,11 @@
+/* Author: Tonmoy Halder
 public class addtwonumbrers {
 
    public static void main(String[] args) {
         
-      int a = 5, b = 15, sum;
-      sum = a + b;
+      int a = 5,    //using int data type and variable name is a
+      int b = 15, sum;  //using int data type and variable name is b
+      sum = a + b; //adding two numbers
 
       System.out.println("Sum of these numbers: "+sum);
    }
